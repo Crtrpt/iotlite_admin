9a4b8d062a4390c8bc59ee50217ff0ffa234db40
jingdor <945627077@qq.com>
2021-03-15 11:54:04 +0800

    '增加c#的mqtt测试'

--- emulator/language/c#/CsharpEmulator/Program.cs
+++ emulator/language/c#/CsharpEmulator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
@@ -16,8 +17,8 @@ namespace CsharpEmulator
         {
             parms = new Dictionary<String,String>();
             parms["broker"] = "";
-            parms["productSn"] = "0001";
-            parms["deviceSn"] = "00001";
+            parms["productSn"] = "language";
+            parms["deviceSn"] = "csharp";
 
             foreach (var item in args)
             {
@@ -34,26 +35,51 @@ namespace CsharpEmulator
             if (client.IsConnected) {
                 Console.WriteLine("connect success");
             }
-            var topic = parms["productSn"] + "/" + parms["deviceSn"];
-            Console.WriteLine("subscribe" + topic);
+            var topic = "/"+parms["productSn"] + "/" + parms["deviceSn"];
+            Console.WriteLine("{0} subscribe topic:{1}", DateTime.Now.ToString(), topic);
             client.Subscribe(new String[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
-
             Console.ReadKey();
-           
         }
 
         private static void Client_MqttMsgSubscribed(object sender, MqttMsgSubscribedEventArgs e)
         {
-            var topic = parms["productSn"] + "/" + parms["deviceSn"];
-            var msg = Encoding.ASCII.GetBytes("hello iot");
-            Console.WriteLine("broker subscrib {0} {1}", topic, System.Text.Encoding.ASCII.GetString(msg));
-            client.Publish(topic, Encoding.ASCII.GetBytes("hello iot"));
+            var topic = parms["productSn"];
+
+            var action = new Action
+            {
+                action = "registered",
+                name="device",
+            };
+            string payload = JsonConvert.SerializeObject(action);
+
+            var msg = Encoding.ASCII.GetBytes(payload);
+            Console.WriteLine("{2} topic:{0} payload:{1}", topic, System.Text.Encoding.ASCII.GetString(msg), DateTime.Now.ToString());
+            client.Publish(topic, msg);
+            var  t = new System.Timers.Timer(5000);
+            t.Elapsed += T_Elapsed;
+            t.AutoReset = true;
+            t.Enabled = true;
+        }
+
+        private static void T_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            var topic = "/" + parms["productSn"] + "/" + parms["deviceSn"];
+            var action = new Action
+            {
+                action = "property",
+                name= "temperature",
+                payload = new Random().Next(1,100)
+            };
+            string payload = JsonConvert.SerializeObject(action);
+
+            var msg = Encoding.ASCII.GetBytes(payload);
+            Console.WriteLine("{2} topic:{0} payload:{1}", topic, System.Text.Encoding.ASCII.GetString(msg), DateTime.Now.ToString());
+            client.Publish(topic, msg);
         }
 
         private static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
         {
-            
-            Console.WriteLine("收到mqtt消息"+ e.Message);
+            Console.WriteLine("{0} topic:{1}  payload:{2}", DateTime.Now.ToString(),e.Topic, Encoding.ASCII.GetString(e.Message));
         }
     }
 }
---
+++ emulator/language/readme.md
@@ -0,0 +1,6 @@
+所有语言都是一个基本的温度计device
+
+
+product language
+
+device 为自己当前的语言 比如 python go java c#
